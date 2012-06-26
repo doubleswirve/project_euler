3b47e6e5196a08cbdccbf8146e0103b93299d021
Saxon Stiller <doubleswirve@gmail.com>
2012-06-26 00:17:44 -0500

    test

--- python/1.py
+++ python/1.py
@@ -1,19 +1,16 @@
-def _function():
-    pass
-
 def sum_mult_3_5(boundary):
-	acc = 0
-	for n in xrange(3, boundary + 1, 3):
-		acc = acc + n
+    acc = 0
+    for n in xrange(3, boundary + 1, 3):
+        acc = acc + n
 
-	for n in xrange(5, boundary + 1, 5):
-		if n % 3 != 0:
-			acc = acc + n
+    for n in xrange(5, boundary + 1, 5):
+        if n % 3 != 0:
+            acc = acc + n
 
-	return acc
+    return acc
 
 def main():
-	print sum_mult_3_5(1000)
+    print sum_mult_3_5(1000)
 
 if __name__ == '__main__':
-	main()
+    main()
