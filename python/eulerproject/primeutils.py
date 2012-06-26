a5579be5539d90a1a37b1ec685d7785d8d1d5cc8
Saxon Stiller <doubleswirve@gmail.com>
2012-06-26 00:24:30 -0500

    update tab

--- python/eulerproject/primeutils.py
+++ python/eulerproject/primeutils.py
@@ -1,17 +1,17 @@
 def next_prime(n):
-	if n < 5:
-		return 3 if n == 2 else 5
+    if n < 5:
+        return 3 if n == 2 else 5
 
-	n       = n + 4 if n % 10 == 3 else n + 2
-	limit   = int(n ** 0.5)
-	divisor = 3
+    n       = n + 4 if n % 10 == 3 else n + 2
+    limit   = int(n ** 0.5)
+    divisor = 3
 
-	while divisor <= limit:
-		if n % divisor == 0:
-			n       = n + 4 if n % 10 == 3 else n + 2
-			limit   = int(n ** 0.5)
-			divisor = 3
-		else:
-			divisor = divisor + 2
+    while divisor <= limit:
+        if n % divisor == 0:
+            n       = n + 4 if n % 10 == 3 else n + 2
+            limit   = int(n ** 0.5)
+            divisor = 3
+        else:
+            divisor = divisor + 2
 
-	return n
+    return n
